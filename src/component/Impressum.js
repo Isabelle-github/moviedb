e482362d1c6d53d1134d85ec39e4497e4bdde512
OlesjaHH <olesja.friesen@gmail.com>
2021-06-30 10:01:13 +0200

    animation imp

--- src/component/Impressum.js
+++ src/component/Impressum.js
@@ -7,9 +7,9 @@ class Impressum extends Component {
             <div className='impressum'>
                 <h1>{getString('impressumTitle')} <span className="animationR">R </span><span className="animationA">A </span><span className="animationC">C </span><span className="animationC2">C </span><span className="animationO">O </span><span className="animationO2">O </span><span className="animationN">N </span> !</h1>
                 <p>{getString('impressumText')}</p>
-                <h3>like this...</h3>
-                <div> 👇🏼 </div>
+                <div className="finger"> 👇🏼 </div>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Nx_Sm8qkj2g?start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+                <div className="raccoon"> 🦝 </div>
             </div>
 
         );
